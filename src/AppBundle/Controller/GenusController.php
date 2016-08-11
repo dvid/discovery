3fc228e26129655ee763b44e429f55e3d516a364
David Polido-Pilo <dvid_pp@yahoo.com>
2016-08-11 15:10:04 +0200

    dynamic load w react js

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -12,35 +12,26 @@ class GenusController extends Controller
     /**
      * @Route("/genus/{genusName}")
      */
-    public function indexAction($genusName)
+    public function showAction($genusName)
     {
-        $notes = [
-            'How fast can it be',
-            'That doesn\'t look like a horse',
-            'So cute'
-        ];
-        return $this->render('genus/show.html.twig', [
+        return $this->render('genus/show.html.twig', array(
             'name' => $genusName,
-            'notes' => $notes
-        ]);
+        ));
     }
-
     /**
-     * @Route("/genus/{genusName}/notes")
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
      * @Method("GET")
      */
-    public function getNotesAction()
+    public function getNotesAction($genusName)
     {
         $notes = [
             ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
             ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
             ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
         ];
-
         $data = [
-            'notes' => $notes,
+            'notes' => $notes
         ];
-
         return new JsonResponse($data);
     }
 }
